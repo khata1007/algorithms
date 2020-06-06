5ed7112ce8820f1f5b1eb79cf3992e2ff3cb1c30
amezaki1007 <mkwamezaki1007@gmail.com>
2020-06-06 11:11:16 +0900

    binary indexed tree

---
+++ tree/BinaryIndexedTree.cpp
@@ -0,0 +1,43 @@
+// 1-index Binary Indexed Tree //
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+struct BinaryIndexedTree{
+private:
+    int n;
+    vector<int> node;
+public:
+    BinaryIndexedTree(){}
+    BinaryIndexedTree(int sz){
+        n = sz+1;
+        node.resize(n, 0);
+    }
+
+    void add(int a, int w){
+        for(int x = a; x <= n; x += x & (-x)) node[x] += w;
+    }
+
+    int sum(int a){
+        int ret = 0;
+        for(int x = a; x > 0; x -= x & (-x)) ret += node[x];
+        return ret;
+    }
+};
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int N; cin >> N;
+    int A[N]; for(int i = 0; i < N; i++) cin >> A[i];
+    
+    BinaryIndexedTree bit(N);
+    for(int i = 0; i < N; i++) bit.add(i+1, A[i]);
+    for(int i = 1; i <= N; i++) cout << bit.sum(i) << endl;
+
+    return 0;
+}
